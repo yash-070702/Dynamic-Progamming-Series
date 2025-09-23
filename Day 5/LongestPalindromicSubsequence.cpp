8a2bd1eb51b4cae17af721b4a38891ed5482a971
Yash Kumar Aggarwal <yashaggarwal2002.ya@gmail.com>
2025-09-23 10:10:32 +0530

    add day 5

--- Day 5/LongestPalindromicSubsequence.cpp
+++ Day 5/LongestPalindromicSubsequence.cpp
@@ -3,6 +3,32 @@ using namespace std;
 
 class Solution {
   public:
+  
+ 
+//<========MEMORIZATION========>
+//   int solve(string &s, int i, int j, vector<vector<int>>& dp) {
+//         // Base cases
+//         if (i > j) return 0;         
+//         if (i == j) return 1;        
+        
+//         if (dp[i][j] != -1) return dp[i][j];
+        
+//         if (s[i] == s[j]) {
+//          
+//             return dp[i][j] = 2 + solve(s, i + 1, j - 1, dp);
+//         } else {
+//
+//             return dp[i][j] = max(solve(s, i + 1, j, dp), solve(s, i, j - 1, dp));
+//         }
+//     }
+    
+//     int longestPalindromeSubseq(string s) {
+//         int n = s.size();
+//         vector<vector<int>> dp(n, vector<int>(n, -1));
+//         return solve(s, 0, n - 1, dp);
+//     }
+
+//<========TABULATION========>
     int solve(string t1,string t2){
    vector<vector<int>>dp(t1.size()+1,vector<int>(t2.size()+1,0));
 
